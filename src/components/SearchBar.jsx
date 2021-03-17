34c6239a71470e4c46a3531f7a86e6dbba0fe088
Irena-Pod <irena.podgurski@gmail.com>
2021-03-17 16:45:47 -0400

    Update search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,13 @@
-import React from "react";
+import React from 'react';
 
 class SearchBar extends React.Component {
-  state = { term: "" };
+  state = { term: '' };
 
   onFormSubmit = (event) => {
     event.preventDefault();
 
     this.props.onSubmit(this.state.term);
+    this.setState({ term: '' });
   };
 
   render() {
@@ -19,6 +20,7 @@ class SearchBar extends React.Component {
               type="text"
               value={this.state.term}
               onChange={(e) => this.setState({ term: e.target.value })}
+              placeholder="Enter search term and hit enter"
             />
           </div>
         </form>
