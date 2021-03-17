cf259375cf7c9bcfe48906822c7930e258d358b6
Irena-Pod <irena.podgurski@gmail.com>
2021-03-17 12:36:49 -0400

    Add ImageList component to render images

---
+++ src/components/ImageList.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable jsx-a11y/alt-text */
+const ImageList = (props) => {
+  const images = props.images.map(({ description, id, urls }) => {
+    return <img alt={description} key={id} src={urls.regular}></img>;
+  });
+  return <div>{images}</div>;
+};
+
+export default ImageList;
