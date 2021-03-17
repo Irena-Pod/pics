9c20d5ba296d187583887c21d51ba18152cba145
Irena-Pod <irena.podgurski@gmail.com>
2021-03-17 16:40:14 -0400

    Add image card component and update styling

--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -1,9 +1,11 @@
-/* eslint-disable jsx-a11y/alt-text */
+import ImageCard from "./ImageCard";
+import "./ImageList.css";
+
 const ImageList = (props) => {
-  const images = props.images.map(({ description, id, urls }) => {
-    return <img alt={description} key={id} src={urls.regular}></img>;
+  const images = props.images.map((image) => {
+    return <ImageCard key={image.id} image={image} />;
   });
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
